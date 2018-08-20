1699a9d1e29bf376fa583d224273c808d1b2641d
ClarkY <cly7100@gmail.com>
2018-08-20 13:24:24 +0100

    database user

--- www/model.py
+++ www/model.py
@@ -217,7 +217,7 @@ class reports(db.Model):
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://ubuntu:ubuntu@localhost:3306/throughmypain'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/throughmypain'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
